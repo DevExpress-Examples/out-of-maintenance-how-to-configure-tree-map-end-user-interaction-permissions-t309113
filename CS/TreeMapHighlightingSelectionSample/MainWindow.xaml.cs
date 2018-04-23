788caa594af7a07b9c957b70b5b2d3284b673ac5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/TreeMapHighlightingSelectionSample/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using Dxtm = DevExpress.Xpf.TreeMap;
+
+namespace TreeMapHighlightingSelectionSample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+
+    public class SelectionModeWrapper {
+        public string ModeName { get; set; }
+        public Dxtm.SelectionMode Mode { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to configure Tree Map end-user interaction permissions
+
+
+This example explains how to configure the end-user interaction permissions.
+
+
+<h3>Description</h3>
+
+To do&nbsp;this, use the <strong>EnableHighlighting</strong> and <strong>SelectionMode</strong> properties.
+
+<br/>
+
+
